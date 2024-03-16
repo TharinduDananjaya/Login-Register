fbb396b44b2db19193e12efd268e0e3483eec615
TharinduDananjaya <115687439+TharinduDananjaya@users.noreply.github.com>
2024-03-16 23:13:25 +0530

    Implemented Home page and route login to Home page.

---
+++ client/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+function Home() {
+  return (    
+      <h1>Home</h1>
+      
+  );
+}
+
+export default Home;
